022f65167da9fd73a5b00ce7ea691fd33275590e
ferhat <ferhatkaplan2742@gmail.com>
2022-09-08 01:06:48 +0300

    use state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
+import react,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const[count,setCount]=useState(4)
+  function incrase(){
+    setCount(oldvalue=> oldvalue+1)
+    setCount(oldvalue=> oldvalue+1)
+  }
+  function descrase(){
+    setCount(oldvalue=> oldvalue-1)
+    setCount(oldvalue=> oldvalue-1)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   <button onClick={descrase}>-</button>
+   <span>{count}</span>
+   <button onClick={incrase}>+</button>
+   </div>
   );
 }
 
